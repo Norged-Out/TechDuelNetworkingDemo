70221ccc2c6daa6b5a0a34187f41a52361ded8a0
Priyansh Nayak <priyansh.nayak.28@gmail.com>
2022-08-27 14:25:11 -0700

    Attempting build push

--- NetworkingDemo/Assets/Script/NewBehaviourScript.cs
+++ NetworkingDemo/Assets/Script/NewBehaviourScript.cs
@@ -13,6 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical")) * 3 * Time.deltaTime);
     }
 }
